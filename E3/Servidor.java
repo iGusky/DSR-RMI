86b17e8a47146c0d0a1f73f47e5993a2535058c0
Radagast <Radagast@localhost.localdomain>
2021-03-04 14:37:10 -0600

    añadiendo servidor

---
+++ E3/Servidor.java
@@ -0,0 +1,27 @@
+package E3;
+ import java.rmi.Naming;
+
+
+public class Servidor{
+    public Servidor(){
+        try{
+            System.setProperty("java.rmi.server.codebase",  "");
+            InterfaceRemota objetoRemoto = new ObjetoRemoto()
+            Naming.rebind("//", objetoRemoto);
+            System.out.println("Iniciando servidor...");
+
+        
+    
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String [] args){
+        new Servidor();
+    }
+
+}
