577cc733eb57760335644af458398624bc220775
cit-arceo <citlalliarceo@gmail.com>
2021-03-05 12:47:05 -0600

    circulo

--- E3/InterfaceRemota.java
+++ E3/InterfaceRemota.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 public interface InterfaceRemota extends Remote {
    public double calcularPoligonoIrregular(ArrayList<Double> array) throws RemoteException;
    public double calcularPoligonoRegular(double numLado, double lado, double apotema) throws RemoteException;
-   public double calcularRadioCirculo(double radio) throws RemoteException;
+   public double calcularCirculo(double radio) throws RemoteException;
 
 }
